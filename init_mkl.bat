df6f8844abb121a7c22c8b776a332eb17aea184c
larssp <lars1@quantentunnel.de>
2017-04-19 15:15:16 +0200

    Update init_mkl.bat

--- init_mkl.bat
+++ init_mkl.bat
@@ -1,4 +1,5 @@
 REM initialize Intel MKL variables
+REM otherwise an error arises: cannot find mkl_intel_thread.dll, even if it is in the PATH
 REM adding run(`mklvars.bat intel64 lp64`) to the julia script does not work
 @echo off
 mklvars.bat intel64 lp64
