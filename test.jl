1419aac52a1bdd1f0cd437461f1834bd9901062f
larssp <lars1@quantentunnel.de>
2017-04-19 15:44:46 +0200

    Update test.jl

--- test.jl
+++ test.jl
@@ -13,16 +13,18 @@ bd_bx  = zeros(ny);
 bd_ay  = zeros(nx);
 bd_by  = zeros(nx);
 
-q = 0.0;
+q      = 0.0;
 BCtype = "DNNN";
+
+# create vector for right hand side
+f      = Array{Cdouble}(Int32((nx+1)*(ny+1)));
+f[:]   = 0.0;
 
 include("mkl_2d_helmholtz_solver.jl")
 
 # INIT CALL
 (ipar, dpar)            = d_init_helmholtz_2d(ax,bx,ay,by,Int32(nx),Int32(ny),BCtype,q);
-# create vector for right hand side
-f                       = Array{Cdouble}(Int32((ipar[13]+1)*(ipar[14]+1)));
-f[:]                    = 0.0;
+
 # COMMIT CALL
 (ipar,dpar,f,xhandle)   = d_commit_helmholtz_2d(f,bd_ax,bd_bx,bd_ay,bd_by,ipar,dpar);
 
@@ -30,6 +32,10 @@ f[:]                    = 0.0;
 gc()
 
 # SOLVE CALL
-# ...
+# the content of "f" is replaced by the desired solution
+# get ipar and xhandle returned, because the memory is freed
+# by a separate function in the shared library
+(ipar,f,xhandle)   = d_helmholtz_2d(f,bd_ax,bd_bx,bd_ay,bd_by,ipar,dpar);
+
 # FREE CALL
-# ...
+free_helmholtz_2d(xhandle,ipar);
